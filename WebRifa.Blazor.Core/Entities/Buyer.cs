bc49710e31ea5cb6ae39ec620b8604d1e18a12e2
Wesley Valentim <wv.reis@gmail.com>
2023-12-31 01:41:15 -0300

    Update method in Buyer.

--- WebRifa.Blazor.Core/Entities/Buyer.cs
+++ WebRifa.Blazor.Core/Entities/Buyer.cs
@@ -5,4 +5,26 @@ public class Buyer : BaseEntity {
     public string Name { get; private set; } = string.Empty;
     public string PhoneNumber { get; private set; } = string.Empty;
     public string Email { get; private set; } = string.Empty;
+
+    public Buyer()
+    {
+            
+    }
+    public void Update(
+        string? name,
+        string? phoneNumber,
+        string? email)
+    {
+        if (!string.IsNullOrEmpty(name)) {
+            Name = name!;
+        }
+
+        if (!string.IsNullOrEmpty(phoneNumber)) {
+            PhoneNumber = phoneNumber!;
+        }
+
+        if (!string.IsNullOrEmpty(email)) {
+            Email = email!;
+        }
+    }
 }
