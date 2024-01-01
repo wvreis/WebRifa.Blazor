a57fad5a545c78c41aa02f7b40aadd635a673752
Wesley Valentim <wv.reis@gmail.com>
2024-01-01 19:00:59 -0300

    Adding logger to methods in controller, but I'll improve that.

--- WebRifa.Blazor/WebRifa.Blazor/Controllers/BuyerController.cs
+++ WebRifa.Blazor/WebRifa.Blazor/Controllers/BuyerController.cs
@@ -21,24 +21,28 @@ public class BuyerController : ControllerBase{
     [HttpGet]
     public async Task<ActionResult<List<BuyerDto>>> SearchBuyer([FromQuery] BuyerSearchQuery query, CancellationToken cancellationToken = default)
     {
+        _logger.LogInformation($"{nameof(SearchBuyer)} executado.");
         return await _buyerService.SearchBuyerAsync(query, cancellationToken);
     }
 
     [HttpGet]
     public async Task<ActionResult<BuyerDto>> GetBuyerAsync([FromQuery] BuyerGetQuery query, CancellationToken cancellationToken = default)
     {
+        _logger.LogInformation($"{nameof(GetBuyerAsync)} executado.");
         return await _buyerService.GetBuyerAsync(query, cancellationToken);
     }
 
     [HttpPost]
     public async Task<ActionResult<Guid>> AddBuyerAsync([FromBody] BuyerDto buyerDto, CancellationToken cancellationToken = default)
     {
+        _logger.LogInformation($"{nameof(AddBuyerAsync)} executado.");
         return await _buyerService.AddBuyerAsync(buyerDto, cancellationToken);
     }
 
     [HttpPut]
     public async Task<ActionResult> UpdateBuyerAsync([FromBody] BuyerDto buyer, CancellationToken cancellationToken = default)
     {
+        _logger.LogInformation($"{nameof(UpdateBuyerAsync)} executado.");
         await _buyerService.UpdateBuyerAsync(buyer, cancellationToken);
         return Ok();
     }
