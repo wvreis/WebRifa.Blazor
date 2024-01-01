f5a65791cea38de801e6b3105d9d836a2f79c6cf
Wesley Valentim <wv.reis@gmail.com>
2024-01-01 18:36:14 -0300

    Adjusting exception middleware.

--- WebRifa.Blazor/WebRifa.Blazor/Exceptions/ExceptionMiddleware.cs
+++ WebRifa.Blazor/WebRifa.Blazor/Exceptions/ExceptionMiddleware.cs
@@ -1,14 +1,16 @@
-﻿using System.Net;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using System.Net;
 
 namespace WebRifa.Blazor.Exceptions;
 public class ExceptionMiddleware {
 
     private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
 
-    public ExceptionMiddleware(RequestDelegate next)
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context)
@@ -24,17 +26,33 @@ public class ExceptionMiddleware {
     private Task HandleExceptionAsync(HttpContext context, Exception ex)
     {
         context.Response.ContentType = "application/json";
-        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Response.StatusCode = GetStatusCodeAsync(ex);
 
         var response = new
         {
             error = new
             {
-                message = "An error occurred while processing your request.",
+                message = $"{ex.GetType().Name}: An error occurred while processing your request.",
                 details = ex.Message
             }
         };
 
+        _logger.LogError(response.ToString());
+
         return context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+    }
+
+    int GetStatusCodeAsync(Exception ex)
+    {
+        switch (ex) {
+            case ArgumentNullException _:
+                return (int)HttpStatusCode.BadRequest;
+            case KeyNotFoundException _:
+                return (int)HttpStatusCode.BadRequest;
+            case UnauthorizedAccessException _:
+                return (int)HttpStatusCode.Unauthorized;
+            default:
+                return (int)HttpStatusCode.InternalServerError;
+        }
     }
 }
